623b48003351299a55f26d7bec7fa80a585a7adc
Kendal Droddy <kdroddy001@gmail.com>
2017-04-18 16:30:47 -0500

    first commit

---
+++ Chapter2/triangle.js
@@ -0,0 +1,17 @@
+/*********************************************************************
+** Author: Kendal Droddy
+**
+** Date: April 18, 2017
+**
+** Challenge: Write a loop that makes seven calls to console.log to
+** output a half triangle shape.
+*********************************************************************/
+
+//Variable to hold sharp symbol
+var sharp = "";
+
+//Loop seven times to output the triangle
+for (var i = 0; i < 7; i++) { 
+  sharp += "#";
+  console.log(sharp);
+}
